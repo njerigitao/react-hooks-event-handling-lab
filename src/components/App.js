40cf2a40497937a1acd2ee6443a46643a750909d
AGNESGITAU <agnesgitau40@gmail.com>
2024-04-27 13:38:07 +0300

    event-handling-App.js

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import EyesOnMe from "./EyesOnMe";
+import Keypad from "./Keypad";
+
+function App(){
+    return (
+        <div>
+            <Keypad />
+            <EyesOnMe />
+        </div>
+    )
+
+}
+export default App
