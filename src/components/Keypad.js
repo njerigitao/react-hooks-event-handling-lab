c85e2816607b5187585c9d46ec7e41232204d5a6
AGNESGITAU <agnesgitau40@gmail.com>
2024-04-27 08:42:55 +0300

    react-events-Keypad.js

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,13 @@
 // Code Keypad Component Here
 
 function Keypad (){
+    const handleChange = (event)=>{
+        console.log('Entering password...');
+    };
     return (
-        <div></div>
+        <div>
+         <Keypad input type="password" onChange={handleChange} />   
+        </div>
     )
 }
 
